d1e3aa84ac644b2219fc14cf85fdbaabb6b429a8
혜민 <97522726+heymin2@users.noreply.github.com>
2022-10-31 11:52:32 +0900

    HTTP Request와 GET/POST

--- diary/auth.py
+++ diary/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/sign-in')
+@auth.route('/sign-in', methods=['GET', 'POST'])
 def sign_in():
     return render_template('sign_in.html', user="SemiCircle")
 
@@ -10,6 +10,29 @@ def sign_in():
 def logout():
     return render_template('logout.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == "POST":     
+        #Check Request data
+        data = request.form
+        
+        #Split Data
+        email = request.form.get('email')
+        nickname = request.form.get('nickname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        #유효성 검사
+        if len(email) < 5:
+            pass
+        elif len(nickname) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            #Create User -> DB
+            pass
+
     return render_template('sign_up.html')
