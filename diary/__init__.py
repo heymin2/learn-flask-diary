0048167a02c670bd2d3f3261a68f4c2418af99c5
혜민 <97522726+heymin2@users.noreply.github.com>
2022-11-02 17:19:09 +0900

    데이터베이스 생성

--- diary/__init__.py
+++ diary/__init__.py
@@ -1,22 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path 
 
 db = SQLAlchemy()
-DB_NAME = 'database.db'
+DB_NAME = "database.db"
+
 # Create App
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'semicircle_secret_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    
+
     #Import Blueprint
     from .views import views
     from .auth import auth
 
     # Apply Blueprint
-    app.register_blueprint(views, ulr_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    #Create or import Database
+    from . import models
+    with app.app_context():
+        db.create_all()
 
     return app
